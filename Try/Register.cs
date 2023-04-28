186895c7b3d82fafac10a979ac0c439d6aca9409
Ксения Герасева <geraseva04@mail.ru>
2023-04-28 15:05:39 +0300

    Добавьте файлы проекта.

---
+++ Try/Register.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+ 
+namespace Register
+{
+    class Register
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст");
+            string str = Console.ReadLine();
+            str = str.ToLower();
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(str[0].ToString().ToUpper());
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (char.IsLetter(str[i]) && char.IsWhiteSpace(str[i - 1]) && ".!?".IndexOf(str[i - 2]) != -1)
+                {
+                    stringBuilder.Append(str[i].ToString().ToUpper());
+                }
+                else
+                {
+                    stringBuilder.Append(str[i]);
+                }
+            }
+            Console.WriteLine(stringBuilder.ToString());
+        }
+    }
+}
